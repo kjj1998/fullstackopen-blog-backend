50a5bb4e0eb66b54745967322d9cc71f8e4807c6
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-11-03 21:15:45 +0800

    Wrote code to test the function that sums up the total number of likes in the array of blogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,22 @@ const dummy = (blogs) => {
 	return 1
 }
 
+const totalLikes = blogs => {
+	if (blogs.length === 0)
+		return 0
+  else if (blogs.length === 1)
+		return blogs[0].likes
+	
+	
+	const reducer = (sum, item) => {
+    return sum + item.likes
+	}
+
+	return blogs.reduce(reducer, 0)
+	
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 }
