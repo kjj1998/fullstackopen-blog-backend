be6324b0385e2fc48c58709e468c6c27472e5825
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-11-03 18:55:08 +0800

    Completed exercise 4.2 of part 4

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
+const app = require('./app') // the actual Express application
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 
-logger.info('connecting to', config.MONGODB_URI)
+const server = http.createServer(app)
 
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-		logger.info('connected to MongoDB')
-	})
-	.catch((error) => {
-		logger.error('error connecting to MongoDB:', error.message)
-	})	
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
-	logger.info(`Server running on port ${config.PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
