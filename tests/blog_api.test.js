44400f1a8874d9b8b853ea37f7857fc063260892
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-11-05 16:12:32 +0800

    Added code to verify that the status code 400 Bad Request is returned if the title and url properties are missing from the request data

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -46,6 +46,38 @@ test('verify that a new blog post is successfully created', async () => {
 	expect(contents).toContain('willremovethissoon')
 })
 
+test('verify that if the likes property is missing from the request, it will default to 0', async () => {
+	const newBlog = { title: 'willremovethissoon', 
+										author: 'John Doe', 
+										url: 'http://noblog.com' 
+									}
+	
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(201)
+		.expect('Content-Type', /application\/json/)
+
+		const blogsAtTheEnd = await helper.blogsInDb()
+		const newlyAddedBlog = blogsAtTheEnd.find(blog =>
+			blog.title === 'willremovethissoon'
+		)
+
+		expect(newlyAddedBlog.likes).toBe(0)
+})
+
+test('verify that if the title and url properties are missing from the request data, responds with status code 400 Bad Request', async () => {
+	const newBlog = { 
+										author: 'John Doe', 
+										likes: 21 
+									}
+	
+	await api
+		.post('/api/blogs')
+		.send(newBlog)
+		.expect(400)
+})
+
 afterAll(() => {
 	mongoose.connection.close()
 })
