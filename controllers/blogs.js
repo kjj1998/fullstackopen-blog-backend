3941b59915c4cfdedf3e700cdf54a4d4109ceb48
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-11-07 22:52:46 +0800

    Refactored the getTokenFrom helper function into a middleware

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -24,9 +24,9 @@ const getTokenFrom = request => {
 blogsRouter.post('/', async (request, response, next) => {
 	try {
 		const body = request.body
-		const token = getTokenFrom(request)
-		const decodedToken = jwt.verify(token, process.env.SECRET)
-		if (!token || !decodedToken.id) {
+		// const token = getTokenFrom(request)
+		const decodedToken = jwt.verify(request.token, process.env.SECRET)
+		if (!decodedToken || !decodedToken.id) {
 			return response.status(401).json({ error: 'token missing or invalid' })
 		}
 		const user = await User.findById(decodedToken.id)
