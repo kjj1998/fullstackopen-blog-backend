080d07f641b361f3ea430cf929b762ced2db8151
Koh Jun Jie <koh.jun.jie@outlook.com>
2021-11-07 21:37:46 +0800

    Added token-based authentication

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -24,6 +25,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
